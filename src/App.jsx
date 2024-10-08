1a9590b23d96d15d4bec8e4902559e356eb77e3d
samuel wanyoike <wanyoikesamuel42@gmail.com>
2024-10-09 02:10:21 +0300

    resolve navbar error

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import Navbar from './components/navbar'
 import HeroSection from './components/HeroSection'
 import FeatureSection from './components/FeatureSection'
 import WorkFlow from './components/WorkFlow'
 import Price from './components/Price'
 import Testimonials from './components/Testimonials'
 import Footer from './components/Footer'
+import Navbar from './components/navbar'
 
 const App = () => {
   return (
     <>
-      <Navbar />
+      <Navbar/>
       <div className='max-w-7xl mx-auto pt-20 px-6'>
         <HeroSection />
         <FeatureSection/>
