75b11ca11c0ed9b02b2eac11214b5335bd55df75
samuel wanyoike <wanyoikesamuel42@gmail.com>
2024-10-09 01:58:12 +0300

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import Navbar from './components/navbar'
+import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
+import WorkFlow from './components/WorkFlow'
+import Price from './components/Price'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <div className='max-w-7xl mx-auto pt-20 px-6'>
+        <HeroSection />
+        <FeatureSection/>
+        <WorkFlow/>
+        <Price/>
+        <Testimonials/>
+        <Footer/>
+      </div>
+     
     </>
   )
 }
